175e3cb4ba5fccdff348f864c0f3afaa05550783
Erik Barcella <57962093+erikbarcella@users.noreply.github.com>
2022-04-24 01:23:01 -0300

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ char pref[10];
 char cia[20];
 
 int menu () {
-    int op=0;
+    int op;
     printf("-------------------------------------------------------- \n");
     printf("Sistema de Controle de Aeroporto de Litoral Norte de RS \n");
     printf("-------------------------------------------------------- \n");
@@ -42,65 +42,60 @@ Aviao *adicionarAviao(Aviao* n) {
         return 0;
 }
 
+
 int main () {
 
     printf("----- Inicio do Programa -----\n");
     
     Aviao * hangar = cria_aviao(hangar);
-   
-   /*  inicializarAvioes();  */
-    int op=0;
-    op=menu();
+    Aviao* l = inicializarAvioes(l);
+    /* hangar=inicializarAvioes(hangar);  */
 
-    do {
-    switch (op) {
-        case 1:
-            printf("[1] Listar todos os avioes em Solo \n");
-            view_aviao(hangar);
-            op=menu();
-            break;
-        case 2:
-            printf("[2] Adicionar aviao em solo\n");
-            if(hangar=adicionarAviao(hangar)){
-                printf("Aviao adicionado com sucesso!!! \n");
-            } else{
-                printf("erro ao adicionar o aviao \n");
-            }
-            op=menu();
-            break;
-        case 3:
-            printf("[3] Remover aviao em solo\n");
-            
-            op=menu();
-            break;
-        case 4:
-            printf("[4] Listar todos os Passageiros\n");
-            
-            op=menu();
-            break;
-        case 5:
-            printf("[5] Adicionar um Passageiro\n");
-            
-            op=menu();
-            break;
-        case 6:
-            printf("[6] Remover um Passageiro\n");
-            
-            op=menu();
-            break;
-        case 7:
-            printf("[7] Pesquisar Passageiro (Idade)\n");
-            
-            op=menu();
-            break;
-        default:
-            printf("Selecione uma opcao valida \n");
-            op=menu();
-            break;
-        }    
-    } while (op!=0);
-    /* encerraAvioes(); */
-    
+    int op;
+   
+    while (op!=0) {
+        op=menu();
+        int aux=op;
+            switch (aux) {
+            case 1:
+                printf("[1] Listar todos os avioes em Solo \n");
+                view_aviao(hangar);
+                break;
+            case 2:
+                printf("[2] Adicionar aviao em solo\n");
+                if(hangar=adicionarAviao(hangar)){
+                    printf("Aviao adicionado com sucesso!!! \n");
+                } else{
+                    printf("erro ao adicionar o aviao \n");
+                }
+                break;
+            case 3:
+                printf("[3] Remover aviao em solo\n");
+                break;
+            case 4:
+                printf("[4] Listar todos os Passageiros\n");
+                break;
+            case 5:
+                printf("[5] Adicionar um Passageiro\n");
+                break;
+            case 6:
+                printf("[6] Remover um Passageiro\n");
+                break;
+            case 7:
+                printf("[7] Pesquisar Passageiro (Idade)\n");
+                break;
+            default:
+                printf("Selecione uma opcao valida \n");
+                break;
+            }   
+    } 
+
+  //salvar arquivos
+    if (salvar_arq(hangar))  {
+        printf("Arquivos salvos com sucesso!! \n");
+    } else  {
+    printf("Erro ao salvar os arquivos \n");
+    }
     return 0;
 }
 
