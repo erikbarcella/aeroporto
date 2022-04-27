5d16acf14572544d14e6a1cf8d52b793224a09b0
erikbarcella <erikbarcellatrisch@gmail.com>
2022-04-27 11:54:56 -0300

    Update passageiro.h

--- passageiro.h
+++ passageiro.h
@@ -2,16 +2,25 @@
 #include<stdio.h>
 #include<string.h>
 
-typedef struct passageiro Passageiro;
-struct passageiro {
-    Passageiro *proximo;
-    Passageiro *anterior;
+struct No{
+    struct No *prox;
+    struct No *ant;
     char nome[80];
     int idade;
     int id;
-    };
+};
+typedef struct No Passageiro;
 
+/*registro do tipo Lista contento dois ponteiros do tipo nó para controlar a lista*/
+struct Lista{
+    struct No *inicio; /*aponta para o elemento do início da lista*/
+    struct No *fim; /*aponta para o elemento do fim da lista*/
+};
+typedef struct Lista Lista;
 
-Passageiro * cria_passageiro(Passageiro *a);
-Passageiro * insere_passageiro(Passageiro * l, char *nome, int ida);
-void view_passageiro(Passageiro* a);
+Lista* cria_lista();
+void insere_lista(Lista *li);
+void imprime_lista_inicio_ao_fim(Lista *li);
+void imprime_lista_fim_ao_inicio(Lista *li);
+void remover_passageiro(Lista *li);
+void find_lista(Lista * l);
